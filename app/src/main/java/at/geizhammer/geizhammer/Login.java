31587313f08e68a5c5bce0933bea4fca5cb2c23b
Zlatko Knežević <zlatknez@gmail.com>
2018-05-21 15:05:35 +0200

    Anmeldeschirm Buttons mit Methoden belegt

--- app/src/main/java/at/geizhammer/geizhammer/Login.java
+++ app/src/main/java/at/geizhammer/geizhammer/Login.java
@@ -2,6 +2,7 @@ package at.geizhammer.geizhammer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    // method for login button
+    public void login(View v){
+        // handle login
+    }
+
+    // method for register button
+    public void register(View v){
+
     }
 }
