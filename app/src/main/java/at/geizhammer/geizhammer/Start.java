142e575e81e15d6bdb1789c441b016c0e0b47b15
zlate42 <zlatknez@gmail.com>
2018-05-21 14:14:54 +0200

    Startbildschirm erledigt

---
+++ app/src/main/java/at/geizhammer/geizhammer/Start.java
@@ -0,0 +1,30 @@
+package at.geizhammer.geizhammer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    // method for login button
+    public void buttonLogin(View view){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+
+    }
+
+    // method for next button
+    public void buttonNext(View view) {
+        Intent intent = new Intent(this, Main.class);
+        startActivity(intent);
+    }
+
+
+}
